0ad94d354b7def1ee3738732b0b4d99afc15e482
Marko Tiitto <Marko.Tiitto@student.oulu.fi>
2026-02-16 14:22:30 +0200

    clean out comments

--- fraud-detection-system/rabbit/consumer/main.py
+++ fraud-detection-system/rabbit/consumer/main.py
@@ -1,8 +1,5 @@
 from rabbitmq import RabbitMQConsumer
 
-# def process_message(message: dict):
-#     print(f"Processing message: {message}")
-
 consumer = RabbitMQConsumer(
     host="rabbitmq",  # Docker service name
     queue_name="CDR",
